b0641a02ce12815a59156742c62a6203bf20b2b9
Grazziano Fagundes <grazzianofagundes@gmail.com>
2022-09-21 16:31:09 -0300

    Implementing form test

--- src/tests/Form.test.tsx
+++ src/tests/Form.test.tsx
@@ -1,8 +1,8 @@
-import { render, screen } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
 import React from 'react';
 import Form from '../components/Form';
 
-test('When the input is empty, new participants cannot be added', () => {
+test('Quando a entrada está vazia, novos participantes não podem ser adicionados', () => {
   render(<Form />);
 
   // Encontra no DOM o input
@@ -17,4 +17,32 @@ test('When the input is empty, new participants cannot be added', () => {
   expect(input).toBeInTheDocument();
   // Garantir que o botão esteja desabilitado
   expect(button).toBeDisabled();
+});
+
+test('Adicionar um participante caso exista um nome preenchido', () => {
+  render(<Form />);
+
+  // Encontra no DOM o input
+  const input = screen.getByPlaceholderText(
+    'Insira os nomes dos participantes'
+  );
+
+  // Encontra o botão
+  const button = screen.getByRole('button');
+
+  // Inserir um valor no input
+  fireEvent.change(input, {
+    target: {
+      value: 'Lara Croft',
+    },
+  });
+
+  // clicar no botão de submter
+  fireEvent.click(button);
+
+  // garantir que o input esta com foco ativo
+  expect(input).toHaveFocus();
+
+  // garantir que o input não tenha um valor
+  expect(input).toHaveValue('');
 });
