da947712f60784270d7c24e74f5e20f4b37f3a57
Grazziano Fagundes <grazzianofagundes@gmail.com>
2022-09-21 23:20:17 -0300

    Last styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { RecoilRoot } from 'recoil';
 import Home from './pages/Home';
+import NameDraw from './pages/NameDraw';
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <RecoilRoot>
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/sorteio" element={<NameDraw />} />
         </Routes>
       </RecoilRoot>
     </BrowserRouter>
--- src/pages/NameDraw/index.tsx
+++ src/pages/NameDraw/index.tsx
@@ -1,6 +1,8 @@
 import React, { useState } from 'react';
+import Card from '../../components/Card';
 import { useListParticipants } from '../../state/hooks/useListParticipants';
 import { useResultadoSorteio } from '../../state/hooks/useResultadoSorteio';
+import './NameDraw.css';
 
 function NameDraw() {
   const participants = useListParticipants();
@@ -19,26 +21,37 @@ function NameDraw() {
   };
 
   return (
-    <section>
-      <form onSubmit={sortear}>
-        <select
-          value={participante}
-          onChange={(event) => setParticipante(event.target.value)}
-          required
-          name="participants"
-          id="participants"
-          placeholder="Selecione o seu nome"
-        >
-          {participants.map((participant) => (
-            <option key={participant} value={participant}>
-              {participant}
-            </option>
-          ))}
-        </select>
-        <button>Sortear</button>
-      </form>
-      {amigo && <p role="alert">{amigo}</p>}
-    </section>
+    <Card>
+      <section className="sorteio">
+        <h2>Quem vai tirar o papelzinho?</h2>
+        <form onSubmit={sortear}>
+          <select
+            value={participante}
+            onChange={(event) => setParticipante(event.target.value)}
+            required
+            name="participants"
+            id="participants"
+            placeholder="Selecione o seu nome"
+          >
+            {participants.map((participant) => (
+              <option key={participant} value={participant}>
+                {participant}
+              </option>
+            ))}
+          </select>
+          <p>Clique em em sortear para ver quem é seu amigo secreto!</p>
+          <button className="botao-sortear">Sortear</button>
+        </form>
+        {amigo && <p role="alert">{amigo}</p>}
+        <footer className="sorteio">
+          <img
+            src="/images/aviao.png"
+            className="aviao"
+            alt="Um desenho de um avião de papel"
+          />
+        </footer>
+      </section>
+    </Card>
   );
 }
 
